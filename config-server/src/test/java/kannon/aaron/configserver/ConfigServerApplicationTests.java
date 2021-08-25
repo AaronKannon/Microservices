ca9f335fd31a55a35d170aa45ac7b2f036d78d13
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-25 15:30:42 -0300

    Adding the config-server folder, the blank template from Spring Initializr

---
+++ config-server/src/test/java/kannon/aaron/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kannon.aaron.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
