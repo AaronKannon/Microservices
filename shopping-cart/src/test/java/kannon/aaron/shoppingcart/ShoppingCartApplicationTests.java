1dca15e3082ea8998a908d5dcb57028f6d309624
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-25 15:27:58 -0300

    Created the Shopping Cart service, utilizing Redis as a database

---
+++ shopping-cart/src/test/java/kannon/aaron/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package kannon.aaron.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
