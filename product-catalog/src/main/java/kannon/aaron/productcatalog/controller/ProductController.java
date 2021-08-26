5dfeffa6a9a67224b528a8ea0f20c6eae1042679
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-26 18:59:17 -0300

    Adding the circuit breaker to the product-catalog and shopping-cart. Adding the functionality to add more amount of the same item.

--- product-catalog/src/main/java/kannon/aaron/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/kannon/aaron/productcatalog/controller/ProductController.java
@@ -4,9 +4,12 @@ package kannon.aaron.productcatalog.controller;
 import kannon.aaron.productcatalog.model.Product;
 import kannon.aaron.productcatalog.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping(value = "/product")
@@ -15,14 +18,37 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+
     @PostMapping
     Product create(@RequestBody Product product) {
-        return productRepository.save(product);
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("product-catalog");
+        Supplier<Product> productSupplier = () -> productRepository.save(product);
+        return circuitBreaker.run(productSupplier, throwable -> handleErrorCreate());
+        //return productRepository.save(product);
     }
 
     @GetMapping(value="/catalog/{id}")
     Optional<Product> findById(@PathVariable Integer id){
-        return productRepository.findById(id);
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("product-catalog");
+        Supplier<Optional<Product>> productSupplier = () -> productRepository.findById(id);
+        return Optional.of(circuitBreaker.run(productSupplier, throwable -> handleErrorFind()).get());
+        //return productRepository.findById(id);
+    }
+
+    private Product handleErrorCreate() {
+        Product error = new Product();
+        error.setName("Error - Didn't Create");
+        error.setAmount(0);
+        return error;
+    }
+
+    private Optional<Product> handleErrorFind() {
+        Optional<Product> error = Optional.of(new Product());
+        error.get().setName("Error - Didn't Found");
+        error.get().setAmount(0);
+        return error;
     }
 
 }
--- shopping-cart/src/main/java/kannon/aaron/shoppingcart/controller/CartController.java
+++ shopping-cart/src/main/java/kannon/aaron/shoppingcart/controller/CartController.java
@@ -4,9 +4,15 @@ import kannon.aaron.shoppingcart.entity.Cart;
 import kannon.aaron.shoppingcart.entity.Item;
 import kannon.aaron.shoppingcart.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping(value = "/cart")
@@ -15,29 +21,60 @@ public class CartController {
     @Autowired
     private CartRepository cartRepository;
 
+    @Autowired
+    private Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
     @PostMapping(value = "/{id}")
     public Cart addItem(@PathVariable("id") Integer id, @RequestBody Item item){
-        Optional<Cart> savedCart = cartRepository.findById(id);
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("shopping-cart");
+        Optional<Cart> savedCart = findById(id);
         Cart cart;
         if(savedCart.equals(Optional.empty())){
             cart = new Cart(id);
         }else{
             cart = savedCart.get();
         }
-        cart.getItems().add(item);
-        return cartRepository.save(cart);
+        boolean check = checkAlreadyExistsItem(cart.getItems().stream().iterator(),item);
+        if (!check) {
+            cart.getItems().add(item);
+        }
+        Supplier<Cart> cartSupplier = () -> cartRepository.save(cart);
+        return circuitBreaker.run(cartSupplier, throwable -> handleCartErrorAddItem());
+        //return cartRepository.save(cart);
     }
 
     @GetMapping(value = "/{id}")
     public Optional<Cart> findById(@PathVariable("id") Integer id){
-
-        return cartRepository.findById(id);
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("shopping-cart");
+        Supplier<Optional<Cart>> cartSupplier = () -> cartRepository.findById(id);
+        return circuitBreaker.run(cartSupplier, throwable -> handleCartErrorFind());
+        //return cartRepository.findById(id);
     }
 
     @DeleteMapping(value = "/{id}")
     public void clear(@PathVariable("id") Integer id){
-
         cartRepository.deleteById(id);
+    }
+
+    private boolean checkAlreadyExistsItem(Iterator<Item> iterator, Item item) {
+        boolean result = false;
+        while (iterator.hasNext()) {
+            Item check = iterator.next();
+            if(check.getProductId().equals(item.getProductId())){
+                int add = check.getAmount();
+                add++;
+                check.setAmount(add);
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private Cart handleCartErrorAddItem() {
+        return null;
+    }
+
+    private Optional<Cart> handleCartErrorFind() {
+        return null;
     }
 }
